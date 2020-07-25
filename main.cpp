b52d0f4658f487b6ad15abdf03c21bfc846800b3
jhye0n <pwn3rhyun@naver.com>
2020-07-25 22:34:14 +0900

    2nd update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <pcap.h>
 #include <libnet.h>
-#include <netinet/in.h>
 #include <stdint.h>
+#include <netinet/in.h>
 
 struct ethernet_hdr
 {
@@ -11,20 +11,20 @@ struct ethernet_hdr
     u_int16_t ether_type;                 /* protocol */
 };
 
+struct ipv4_hdr
+{
+	struct in_addr ip_src, ip_dst;
+};
+
+
 int main(int argc, char *argv[]){
-	if(argc != 2){
+	if(argc < 2){
 		printf("usage %s <network-interface>\n",argv[0]);
 		return 0;
 	}
 	
 	char *dev = argv[1];
 	char errbuf[PCAP_ERRBUF_SIZE];
-	struct pcap_pkthdr *pkthdr;
-	struct ethernet_hdr *eth_hdr;
-	struct ipv4_hdr *ip_hdr;
-	const u_char *pkdata;
-	int i=0,j=0;
-	uint8_t k=0;
 	pcap_t *handle;
 
 	handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
@@ -33,26 +33,41 @@ int main(int argc, char *argv[]){
 		return 0;
 	}
 
-	while(1){
-		int res = pcap_next_ex(handle, &pkthdr, &pkdata);
+	while(true){
+		struct pcap_pkthdr* header;
+		struct ethernet_hdr *eth_hdr;
+		struct ipv4_hdr *ipv4_hdr;
+		const u_char* packet;
+		int res = pcap_next_ex(handle, &header, &packet);
 		if(res == 0){
 			continue;
 		}else if(res == -1 || res == -2){
-			fprintf(stderr, "pcap packet %s",pcap_geterr(handle));
+			printf("pcap packet %s",pcap_geterr(handle));
 			exit(-1);
 		}
 
-		printf("%u bytes captured\n", pkthdr->caplen);
-	
-		for(i=0; i<ETHER_ADDR_LEN; i++){
+		printf("%u bytes captured\n", header->caplen);
+		printf("\n--ethernet Header--\n");
+
+		eth_hdr = (struct ethernet_hdr *) packet;
+		for(int i=0; i<ETHER_ADDR_LEN; i++){
 			printf("%02x:", eth_hdr->ether_dhost[i]);
 		}
-		printf("-->");
-		for(j=0; j<ETHER_ADDR_LEN; j++){
+		printf("\n");
+		for(int j=0; j<ETHER_ADDR_LEN; j++){
 			printf("%02x:", eth_hdr->ether_shost[j]);
 		}
-		printf("\n");
+
+		printf("\n--Ipv4 Header--\n");
+		packet += sizeof(struct ethernet_hdr);
+		ipv4_hdr = (struct ipv4_hdr *) packet;
+		printf("src : %s\n", inet_ntoa(ipv4_hdr->ip_src));
+		printf("dst : %s\n", inet_ntoa(ipv4_hdr->ip_dst));
 		
+		printf("\n--tcp header--\n");
+		printf("test\n");
+
+
 	}
 
 
